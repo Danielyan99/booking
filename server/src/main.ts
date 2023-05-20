9d59db81f0de48815cf0dd385d59ace069e4568e
Narek Danielyan <danielyan.narek99@gmail.com>
2023-05-20 21:23:44 +0400

    limit added to server

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true });
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({ credentials: true, origin: process.env.CLIENT_URI });
+  app.useBodyParser('json', { limit: '10mb' });
 
   await app.listen(4000);
 }
